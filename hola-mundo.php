726baca90075a1c2cface3d519b8e45f9b37263e
Brent C <educate-sysadmin@ucl.ac.uk>
2018-02-20 11:20:42 +0000

    Initial commit

---
+++ hola-mundo.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Hola Mundo
+Plugin URI: https://github.com/educate-sysadmin/holamundo
+Description: Hello world test plugin
+Version: 0.1
+Author: b.cunningham@ucl.ac.uk
+Author URI: https://educate.london
+License: GPL2
+*/
